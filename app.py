3bd1caf27b6f54910b42be72fa5d32b601efd473
riduanmod <riduanmod@gmail.com>
2026-04-12 16:06:04 +0600

    Refactor binary payload creation and update response

--- app.py
+++ app.py
@@ -75,7 +75,20 @@ def login_process(uid, password, region="BD"):
             url, host = "https://loginbp.ggblueshark.com/MajorLogin", "loginbp.ggblueshark.com"
         lang = REGION_LANG.get(region, "en")
         
-        binary_head = b'\x1a\x132025-08-30 05:19:21"\tfree fire(\x01:\x081.120.13B2Android OS 9 / API-28 (PI/rel.cjw.20220518.114133)J\x08HandheldR\nATM MobilsZ\x04WIFI`\xb6\nh\xee\x05r\x03300z\x1fARMv7 VFPv3 NEON VMH | 2400 | 2\x80\x01\xc9\x0f\x8a\x01\x0fAdreno (TM) 640\x92\x01\rOpenGL ES 3.2\x9a\x01+Google|dfa4ab4b-9dc4-454e-8065-e70c733fa53f\xa2\x01\x0e105.235.139.91\xaa\x01\x02'
+        # --- ডাইনামিক বাইনারি পেলোড তৈরি (Protobuf Format) ---
+        part1 = b'\x1a\x132025-08-30 05:19:21"\tfree fire(\x01'
+        
+        client_version_bytes = CLIENT_VERSION.encode("ascii")
+        version_payload = b':' + bytes([len(client_version_bytes)]) + client_version_bytes
+        
+        os_info = f"{ANDROID_OS_VERSION} / API-28 (PI/rel.cjw.20220518.114133)"
+        os_info_bytes = os_info.encode("ascii")
+        os_payload = b'B' + bytes([len(os_info_bytes)]) + os_info_bytes
+        
+        part2 = b'J\x08HandheldR\nATM MobilsZ\x04WIFI`\xb6\nh\xee\x05r\x03300z\x1fARMv7 VFPv3 NEON VMH | 2400 | 2\x80\x01\xc9\x0f\x8a\x01\x0fAdreno (TM) 640\x92\x01\rOpenGL ES 3.2\x9a\x01+Google|dfa4ab4b-9dc4-454e-8065-e70c733fa53f\xa2\x01\x0e105.235.139.91\xaa\x01\x02'
+        
+        binary_head = part1 + version_payload + os_payload + part2
+        
         binary_tail = b'\xb2\x01 1d8ec0240ede109973f3321b9354b44d\xba\x01\x014\xc2\x01\x08Handheld\xca\x01\x10Asus ASUS_I005DA\xea\x01@afcfbf13334be42036e4f742c80b956344bed760ac91b3aff9b607a610ab4390\xf0\x01\x01\xca\x02\nATM Mobils\xd2\x02\x04WIFI\xca\x03 7428b253defc164018c604a1ebbfebdf\xe0\x03\xa8\x81\x02\xe8\x03\xf6\xe5\x01\xf0\x03\xaf\x13\xf8\x03\x84\x07\x80\x04\xe7\xf0\x01\x88\x04\xa8\x81\x02\x90\x04\xe7\xf0\x01\x98\x04\xa8\x81\x02\xc8\x04\x01\xd2\x04=/data/app/com.dts.freefireth-PdeDnOilCSFn37p1AH_FLg==/lib/arm\xe0\x04\x01\xea\x04_2087f61c19f57f2af4e7feff0b24d9d9|/data/app/com.dts.freefireth-PdeDnOilCSFn37p1AH_FLg==/base.apk\xf0\x04\x03\xf8\x04\x01\x8a\x05\x0232\x9a\x05\n2019119621\xb2\x05\tOpenGLES2\xb8\x05\xff\x7f\xc0\x05\x04\xe0\x05\xf3F\xea\x05\x07android\xf2\x05pKqsHT5ZLWrYljNb5Vqh//yFRlaPHSO9NWSQsVvOmdhEEn7W+VHNUK+Q+fduA3ptNrGB0Ll0LRz3WW0jOwesLj6aiU7sZ40p8BfUE/FI/jzSTwRe2\xf8\x05\xfb\xe4\x06\x88\x06\x01\x90\x06\x01\x9a\x06\x014\xa2\x06\x014\xb2\x06"GQ@O\x00\x0e^\x00D\x06UA\x0ePM\r\x13hZ\x07T\x06\x0cm\\V\x0ejYV;\x0bU5'
         
         full_payload = binary_head + lang.encode("ascii") + binary_tail
@@ -211,7 +224,6 @@ def get_clan_info():
                 except:
                     officers_list = response_message.big_numbers
 
-                # ডিকশনারিতে মূল পরিবর্তনগুলো করা হয়েছে
                 formatted_response = {
                     "Developer": "Riduanul Islam",
                     "TelegramBot": "https://t.me/RiduanFFBot",
@@ -235,9 +247,8 @@ def get_clan_info():
                     },
                     "notice": response_message.welcome_message,
                     
-                    # লিডারের তথ্য এখানে আপডেট করা হয়েছে
-                    "guild_leader": response_message.status_code,  # মূল গিল্ড লিডারের আইডি (Field 5)
-                    "acting_leader": response_message.value_a,     # অ্যাক্টিং/ভারপ্রাপ্ত লিডারের আইডি (Field 4)
+                    "guild_leader": response_message.status_code,
+                    "acting_leader": response_message.value_a,
                     "guild_officers": officers_list,
                     
                     "system_info": {
